ae27f9e64fb91fed6c23700d516966a68389d2f8
Muhammad Azka <azkabdg@gmail.com>
2021-05-08 17:35:39 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const Hapi = require('@hapi/hapi');
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost',
+});
+
+const init = async () => {
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+init();
